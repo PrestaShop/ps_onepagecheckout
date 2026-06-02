272f85a147ee40c089e41d5f8e229f55eaf923ba
ThbPS <thomas.broussoux@prestashop.com>
2026-06-02 13:06:56 +0200

    chore: prepare 0.2.0 beta release

--- ps_onepagecheckout.php
+++ ps_onepagecheckout.php
@@ -27,7 +27,7 @@ class Ps_Onepagecheckout extends Module
     {
         $this->name = 'ps_onepagecheckout';
         $this->tab = 'front_office_features';
-        $this->version = '0.1.0';
+        $this->version = '0.2.0';
         $this->author = 'PrestaShop';
         $this->need_instance = 0;
         $this->bootstrap = true;
