7135e3554f0104caa725b02b74cc9b616eff429f
L3RAZ <laurynas.sedys@invertus.eu>
2026-06-10 11:49:18 +0300

    CS fix

--- ps_onepagecheckout.php
+++ ps_onepagecheckout.php
@@ -12,10 +12,11 @@ if (file_exists(__DIR__ . '/vendor/autoload.php')) {
     require __DIR__ . '/vendor/autoload.php';
 }
 
-if (class_exists(\Symfony\Component\Dotenv\Dotenv::class) && file_exists(_PS_MODULE_DIR_ . 'ps_onepagecheckout/.env')) {
+if (class_exists(Symfony\Component\Dotenv\Dotenv::class) && file_exists(_PS_MODULE_DIR_ . 'ps_onepagecheckout/.env')) {
     try {
-        (new \Symfony\Component\Dotenv\Dotenv())->load(_PS_MODULE_DIR_ . 'ps_onepagecheckout/.env');
-    } catch (\Throwable $e) {}
+        (new Symfony\Component\Dotenv\Dotenv())->load(_PS_MODULE_DIR_ . 'ps_onepagecheckout/.env');
+    } catch (Throwable $e) {
+    }
 }
 
 use PrestaShop\Module\PsOnePageCheckout\Analytics\Analytics;
