88634f1f756010a12ea8a62e253c75f27bb11572
L3RAZ <laurynas.sedys@invertus.eu>
2026-06-17 16:29:10 +0300

    Bumbed module version

--- ps_onepagecheckout.php
+++ ps_onepagecheckout.php
@@ -28,7 +28,7 @@ class Ps_Onepagecheckout extends Module
     {
         $this->name = 'ps_onepagecheckout';
         $this->tab = 'front_office_features';
-        $this->version = '0.4.0';
+        $this->version = '0.5.0';
         $this->author = 'PrestaShop';
         $this->need_instance = 0;
         $this->bootstrap = true;
