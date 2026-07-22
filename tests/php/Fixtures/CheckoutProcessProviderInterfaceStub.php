66435461eebbdeae19f0be0f10990e7729ac679e
Laurynas Šedys <laurynas.sedys.96@gmail.com>
2026-07-22 17:23:48 +0300

    Reverted Interface change (#125)

--- tests/php/Fixtures/CheckoutProcessProviderInterfaceStub.php
+++ tests/php/Fixtures/CheckoutProcessProviderInterfaceStub.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace PrestaShop\PrestaShop\Adapter\Order\Checkout;
 
-use Symfony\Contracts\Translation\TranslatorInterface;
+use PrestaShopBundle\Translation\TranslatorComponent;
 
 if (!interface_exists(CheckoutProcessProviderInterface::class)) {
     interface CheckoutProcessProviderInterface
@@ -13,7 +13,7 @@ if (!interface_exists(CheckoutProcessProviderInterface::class)) {
 
         public function buildCheckoutProcess(
             \CheckoutSession $session,
-            TranslatorInterface $translator,
+            TranslatorComponent $translator,
         ): \CheckoutProcess;
     }
 }
